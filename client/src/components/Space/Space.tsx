16d596c70b818faba7f3a67e3a621c06dd6747fe
Leo Fleury <leofleury72@gmail.com>
2026-05-28 21:35:32 +0200

    create Space section

---
+++ client/src/components/Space/Space.tsx
@@ -0,0 +1,17 @@
+import CardSpace from "./CardSpace/CardSpace";
+import "./Space.css";
+function Space() {
+  return (
+    <section className="sectionSpace">
+      <h5>Espaces</h5>
+      <div className="Title">
+        <h2>Des lieux pour chaque usage</h2>
+        <p>Tous les espaces</p>
+      </div>
+
+      <CardSpace />
+    </section>
+  );
+}
+
+export default Space;
