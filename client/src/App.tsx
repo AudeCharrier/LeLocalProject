058fa2d29d7b2cf5a0b68a48978280227d1d8b28
Brice Kutuk <bricekutuk@live.fr>
2026-05-29 10:36:07 +0200

    transfer the page app to home

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,7 @@
 import "./App.css";
-import DescriptifAccueil from "./components/DescriptifAccueil/DescriptifAccueil";
-import ImageHautPageAcceuil from "./components/ImageHautPageAcceuil/ImageHautPageAcceuil";
-import NavBar from "./components/NavBar/NavBar";
-import PresentationAccueil from "./components/PresentationAcceuil/PresentationAccueil";
 
 function App() {
-  return (
-    <>
-      <NavBar />
-      <ImageHautPageAcceuil />
-      <PresentationAccueil />
-      <DescriptifAccueil />
-    </>
-  );
+  return <></>;
 }
 
 export default App;
--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,8 +1,16 @@
+import DescriptifAccueil from "../../components/DescriptifAccueil/DescriptifAccueil";
+import ImageHautPageAcceuil from "../../components/ImageHautPageAcceuil/ImageHautPageAcceuil";
+import NavBar from "../../components/NavBar/NavBar";
+import PresentationAccueil from "../../components/PresentationAcceuil/PresentationAccueil";
 import Space from "../../components/Space/Space";
 import "./Home.css";
 function Home() {
   return (
     <section className="home-section">
+      <NavBar />
+      <ImageHautPageAcceuil />
+      <PresentationAccueil />
+      <DescriptifAccueil />
       <Space />
     </section>
   );
