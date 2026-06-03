d4d6ab0895cd30746f082ef24a19dac51968418f
Lucas Guilhot <143024831+lucas04022002@users.noreply.github.com>
2026-06-03 16:56:20 +0200

    add footer

---
+++ client/src/components/FooterDashboard/FooterDashboard.tsx
@@ -0,0 +1,9 @@
+function FooterDashboard() {
+  return (
+    <footer className="footer-dashboard">
+      <p>© 2026 Le Local — Association loi 1901 · Mentions légales </p>
+    </footer>
+  );
+}
+
+export default FooterDashboard;
---
+++ client/src/components/FooterHome/FooterHome.tsx
@@ -0,0 +1,9 @@
+function FooterHome() {
+  return (
+    <footer className="footer-home">
+      <p>© 2026 Le Local — Association loi 1901 · Mentions légales </p>
+    </footer>
+  );
+}
+
+export default FooterHome;
