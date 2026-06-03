3f33e44f0b6d222ef9a07d7797bc2eac082b7671
Nico Semenadisse <nicolas.semenadisse@hotmail.com>
2026-06-03 16:24:44 +0200

    create:route to /espaces

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom/client";
 
 import { RouterProvider, createBrowserRouter } from "react-router";
 import App from "./App";
-import Space from "./components/Space/Space";
 import Home from "./pages/Home/Home";
+import Spaces from "./pages/Spaces/Spaces";
 
 // Pages
 
@@ -18,8 +18,8 @@ const router = createBrowserRouter([
         element: <Home />,
       },
       {
-        path: "/space",
-        element: <Space />,
+        path: "/espaces",
+        element: <Spaces />,
       },
     ],
   },
