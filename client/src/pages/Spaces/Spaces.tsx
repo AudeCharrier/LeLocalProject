8a64a8f912b26f92cd0ac281ceabd4237832f1a5
Nico Semenadisse <nicolas.semenadisse@hotmail.com>
2026-06-03 15:50:53 +0200

    create: Spaces page, Header component and Header css

---
+++ client/src/pages/Spaces/Spaces.tsx
@@ -0,0 +1,12 @@
+import "./Spaces.css";
+import Header from "../../components/Spaces/Header/Header";
+
+function Spaces() {
+  return (
+    <section className="spaces-global-section">
+      <Header />
+    </section>
+  );
+}
+
+export default Spaces;
