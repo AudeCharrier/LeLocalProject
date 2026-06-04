2315cc6684af0a22ceabf34723fbc71779e349c9
Nico Semenadisse <nicolas.semenadisse@hotmail.com>
2026-06-04 12:31:52 +0200

    fix: error on label

--- client/src/components/SpacesPage/Header/Header.tsx
+++ client/src/components/SpacesPage/Header/Header.tsx
@@ -1,32 +1,15 @@
+import FirstArticle from "./FirstArticle/FirstArticle";
 import "./Header.css";
+import SecondArticle from "./SecondArticle/SecondArticle";
 
+//Header creation
 function Header() {
   return (
     <section className="header-spaces-page-global-section">
+      {/* Global Header */}
       <div className="header-spaces-page-container-articles">
-        <article className="header-spaces-page-first-article">
-          <div className="header-spaces-page-first-article-main-div">
-            <div className="header-spaces-page-first-article-main-span-div">
-              <hr className="header-spaces-page-first-article-main-hr" />
-
-              <span className="header-spaces-page-first-article-main-span">
-                NOS LIEUX DE CRÉATION
-              </span>
-            </div>
-            <h2 className="header-spaces-page-first-article-main-title">
-              Des{" "}
-              <em className="header-spaces-page-first-article-main-em">
-                espaces
-              </em>{" "}
-              à votre image
-            </h2>
-            <p className="header-spaces-page-first-article-main-text">
-              Coworking, studios son & photo, salle de réunion, atelier
-              fabrication… Réservez à l'heure, à la demi-journée ou par
-              abonnement mensuel.
-            </p>
-          </div>
-        </article>
+        <FirstArticle />
+        <SecondArticle />
       </div>
     </section>
   );
