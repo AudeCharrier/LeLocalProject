33cb068b38023e0fccb6cdce15b0dca5ad43ae9c
Nico Semenadisse <nicolas.semenadisse@hotmail.com>
2026-06-04 16:36:25 +0200

    feature:add CardSpace on Body.tsx, and adjust css

--- client/src/components/SpacesPage/Body/Body.tsx
+++ client/src/components/SpacesPage/Body/Body.tsx
@@ -1,15 +1,16 @@
+import CardSpace from "../../Space/CardSpace/CardSpace";
 import "./Body.css";
 
 function Body() {
   return (
     <section className="body-spaces-page-global-section">
       <div className="body-spaces-page-container-div">
-        <div className="body-spaces-page-global-container-title-div">
-          <h2 className="body-spaces-page-global-container-title">
-            {" "}
-            NOS ESPACES
-          </h2>
-          <hr />
+        <div className="body-spaces-title-div">
+          <h2 className="body-spaces-title"> NOS ESPACES</h2>
+          <hr className="body-spaces-page-hr" />
+        </div>
+        <div className="body-spaces-page-spaces-list-div">
+          <CardSpace />
         </div>
       </div>
     </section>
