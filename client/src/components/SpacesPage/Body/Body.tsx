4857b4479b2a46f3c25f4f89dc16a10a4216e117
Nico Semenadisse <nicolas.semenadisse@hotmail.com>
2026-06-04 15:45:30 +0200

    create:Body composant

---
+++ client/src/components/SpacesPage/Body/Body.tsx
@@ -0,0 +1,19 @@
+import "./Body.css";
+
+function Body() {
+  return (
+    <section className="body-spaces-page-global-section">
+      <div className="body-spaces-page-container-div">
+        <div className="body-spaces-page-global-container-title-div">
+          <h2 className="body-spaces-page-global-container-title">
+            {" "}
+            NOS ESPACES
+          </h2>
+          <hr />
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Body;
