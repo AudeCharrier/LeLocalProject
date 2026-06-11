a6ea135edae5b8b9fdd6ec634ceb7b7b222e3576
Lucas Guilhot <143024831+lucas04022002@users.noreply.github.com>
2026-06-11 16:17:09 +0200

    modif

--- client/src/pages/Spaces/Spaces.tsx
+++ client/src/pages/Spaces/Spaces.tsx
@@ -1,12 +1,14 @@
 import "./Spaces.css";
 import Body from "../../components/SpacesPage/Body/Body";
 import Header from "../../components/SpacesPage/Header/Header";
+import FooterDashboard from "../../components/FooterDashboard/FooterDashboard";
 
 function Spaces() {
   return (
     <section className="spaces-global-section">
       <Header />
       <Body />
+      <FooterDashboard />
     </section>
   );
 }
