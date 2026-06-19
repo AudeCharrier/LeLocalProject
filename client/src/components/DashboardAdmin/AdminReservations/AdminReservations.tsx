07c4efa36f7447ce8c7dd7d12bde6536f6729b1e
Lucas Guilhot <143024831+lucas04022002@users.noreply.github.com>
2026-06-19 12:20:24 +0200

    fix ortho

--- client/src/components/DashboardAdmin/AdminReservations/AdminReservations.tsx
+++ client/src/components/DashboardAdmin/AdminReservations/AdminReservations.tsx
@@ -40,7 +40,7 @@ function AdminReservations() {
                 <th>Membre</th>
                 <th>Espace</th>
                 <th>Date</th>
-                <th>Créneau</th>
+                <th>Créneaux</th>
                 <th>Montant</th>
                 <th>Statut</th>
               </tr>
