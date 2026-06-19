abd557f7e466defda11f939580903cf7b17f3062
Nico Semenadisse <nicolas.semenadisse@hotmail.com>
2026-06-19 10:15:45 +0200

    fix: show available timeslot when not isOpenSpace

--- client/src/components/SpacesPage/Body/SpaceModal/BookingForm/BookingForm.tsx
+++ client/src/components/SpacesPage/Body/SpaceModal/BookingForm/BookingForm.tsx
@@ -157,27 +157,29 @@ function BookingForm({ space, onBack, userId }: BookingFormProps) {
   if (success) {
     return (
       <div className="booking-form">
+        <button type="button" className="booking-form-back" onClick={onBack}>
+          ‹ Retour
+        </button>
         <h2 className="booking-form-title">Ajouté au panier</h2>
         <p>
           Votre réservation pour {space.space_name} le {formattedDate} a été
           ajoutée à votre panier.
         </p>
         <div className="booking-form-button-div">
-          <button
-            type="button"
-            className="booking-form-go-cart"
-            onClick={() => navigate("/cart")}
-          >
-            ‹ Voir votre panier
-          </button>
-          <button type="button" className="booking-form-back" onClick={onBack}>
-            ‹ Retour
-          </button>
+          <div className="booking-form-button-div">
+            <button
+              type="button"
+              className="booking-form-go-cart"
+              onClick={() => navigate("/cart")}
+            >
+              ‹ Voir votre panier
+            </button>
+          </div>
         </div>
       </div>
     );
   }
-
+  console.log(availability);
   return (
     <form className="booking-form" onSubmit={handleSubmit}>
       <button type="button" className="booking-form-back" onClick={onBack}>
@@ -244,9 +246,11 @@ function BookingForm({ space, onBack, userId }: BookingFormProps) {
           {availabilityLoading
             ? "Vérification des disponibilités..."
             : availability
-              ? (availability?.available ?? 0) > 0
-                ? `${availability.available} place${(availability?.available ?? 0) > 1 ? "s" : ""} disponible${(availability?.available ?? 0) > 1 ? "s" : ""} sur ${availability.capacity}`
-                : "Aucune place disponible pour ce créneau"
+              ? isOpenSpace
+                ? (availability?.available ?? 0) > 0
+                  ? `${availability.available} place${(availability?.available ?? 0) > 1 ? "s" : ""} disponible${(availability?.available ?? 0) > 1 ? "s" : ""} sur ${availability.capacity}`
+                  : "Aucune place disponible pour ce créneau"
+                : null
               : null}
         </span>
       )}
